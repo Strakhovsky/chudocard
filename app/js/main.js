600a4bf4de767fd1866798f7564d5e55e81dd29c
Dmitriy Maruschack notebook <dmitriymaruschack@gmail.com>
2018-02-06 17:47:39 +0300

    with beta modal-menu and some logo fix

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,15 @@ $(function() {
       trigger: 'click'
     }); 
   })
+
+// Modal menu
+/*
+$(".modal-transparent").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+  }, 0);
+});
+$(".modal-transparent").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-transparent");
+});
+*/

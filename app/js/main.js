0c5fbcc295e7f4281dc58eb3e8a1a4bea98bd642
Serhii <sergey.anistratenko@popel.com.ua>
2018-02-28 21:44:05 +0200

    Session

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 // PopOver
 
+$.fn.modal.Constructor.TRANSITION_DURATION = 600
+$.fn.modal.Constructor.BACKDROP_TRANSITION_DURATION = 650
+
 $(function() {
     // Включаем поповер везде, где есть атрибут data-toggle="popover"
     $('[data-toggle="popover"]').popover({
@@ -13,65 +16,28 @@ $(function() {
 
 // Modal menu
 
-$(".modal-menu").on('show.bs.modal', function () {
-  setTimeout( function() {
-    $(".modal-backdrop").addClass("modal-backdrop-transparent");
-  }, 0);
-/*
-  setTimeout( function() {
-    $("div.point-box1").addClass('point-box-1');
-  }, 400);
-
-  setTimeout( function() {
-    $("div.point-box2").addClass('point-box-1');
-  }, 600);
-
-  setTimeout( function() {
-    $("div.point-box3").addClass('point-box-1');
-  }, 800);
-
-  setTimeout( function() {
-    $("div.point-box4").addClass('point-box-1');
-  }, 1000);
-
-  setTimeout( function() {
-    $("div.point-box5").addClass('point-box-1');
-  }, 1200);
-
-  setTimeout( function() {
-    $("div.point-box6").addClass('point-box-1');
-  }, 1400);
-
-  setTimeout( function() {
-    $("div.point-box7").addClass('point-box-1');
-  }, 1600);
-
-  setTimeout( function() {
-    $("div.point-box8").addClass('point-box-1');
-  }, 1800);
-
-  setTimeout( function() {
-    $("div.point-box9").addClass('point-box-1');
-  }, 2000);
-*/
-});
-
 $("#modal-menu").on('hide.bs.modal', function () {
-    /*
-    setTimeout(function() {
-      $('div.point-box1').removeClass("point-box-1");
-      $('div.point-box1').addClass('point-test');
-    }, 2000);
-    alert(1000);
-    $("div.point-box1").removeClass("point-box-1");
-    $("div.point-box2").removeClass("point-box-1");
-    $("div.point-box2").removeClass("point-box-1");
-    $("div.point-box3").removeClass("point-box-1");
-    $("div.point-box4").removeClass("point-box-1");
-    $("div.point-box5").removeClass("point-box-1");
-    $("div.point-box6").removeClass("point-box-1");
-    $("div.point-box7").removeClass("point-box-1");
-    $("div.point-box8").removeClass("point-box-1");
-    */
+
 });
 
+$(document).on('click', '[data-click]', function (e) {
+  e.preventDefault();
+
+  var targetSelector = $(this).data('click');
+
+  console.log(targetSelector);
+  console.log($(targetSelector))
+
+  $(targetSelector).trigger('click');
+});
+
+$(document).on('change', '#checkbox-reg', function () {
+  console.log($(this).prop('checked'));
+
+  if ($(this).prop('checked')) {
+    $('.registration').addClass('ready');
+  } else  {
+    $('.registration').removeClass('ready');
+  }
+  
+})
